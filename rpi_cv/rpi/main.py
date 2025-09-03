ea5c70345afc8290df80ea3adcce24f28c912eea
Sean <jenseanfoo@gmail.com>
2025-09-03 15:31:45 +0800

    fix: settings for android

--- rpi_cv/rpi/main.py
+++ rpi_cv/rpi/main.py
@@ -1,10 +1,14 @@
 from link.android_link import AndroidLink
 from link.stm32_link import STMLink
-
+from time import sleep
 
 def main():
     android_link = AndroidLink()
-    stm_link = STMLink()
+    android_link.connect()
+
+    sleep(30)
+
+    # stm_link = STMLink()
 
 
 if __name__ == "__main__":
