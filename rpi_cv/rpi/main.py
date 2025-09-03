c9ef88b4073b8ecffcceb0ee42d0a35ea3e49840
50Fifty <jenseanfoo@gmail.com>
2025-09-03 15:35:09 +0800

    feat: add AndroidMessage sending functionality in main

--- rpi_cv/rpi/main.py
+++ rpi_cv/rpi/main.py
@@ -2,10 +2,15 @@ from link.android_link import AndroidLink
 from link.stm32_link import STMLink
 from time import sleep
 
+from message.android import AndroidMessage
+
+
 def main():
     android_link = AndroidLink()
     android_link.connect()
 
+    msg = AndroidMessage(cat="info", value="Hello from RPi!")
+    android_link.send(msg)
     sleep(30)
 
     # stm_link = STMLink()
