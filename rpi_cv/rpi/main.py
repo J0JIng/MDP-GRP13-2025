86c161685f48fde3f8dd9391762974460d8ab46f
50Fifty <jenseanfoo@gmail.com>
2025-09-17 16:38:38 +0800

    fix: add buffer clearing and print received message in main function

--- rpi_cv/rpi/main.py
+++ rpi_cv/rpi/main.py
@@ -11,6 +11,8 @@ def main():
 
     msg = AndroidMessage(cat="info", value="Hello from RPi!")
     android_link.send(msg)
+    recv = android_link.recv()  # Just to clear the buffer
+    print(f"Received: {recv}")
     android_link.disconnect()
     sleep(30)
 
