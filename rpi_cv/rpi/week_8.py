3b9623af70b7861dfb56bf17df882edc34189333
50Fifty <jenseanfoo@gmail.com>
2025-10-02 11:36:29 +0800

    refactor: replace rs_flag with expecting_rs_ack for better acknowledgment handling in RaspberryPi

--- rpi_cv/rpi/week_8.py
+++ rpi_cv/rpi/week_8.py
@@ -58,7 +58,7 @@ class RaspberryPi:
         self.proc_android_sender = None
         self.proc_command_follower = None
         self.proc_rpi_action = None
-        self.rs_flag = False
+        self.expecting_rs_ack = self.manager.Value('b', False)
         self.success_obstacles = self.manager.list()
         self.failed_obstacles = self.manager.list()
         self.obstacles = self.manager.dict()
@@ -267,8 +267,8 @@ class RaspberryPi:
                 continue
 
             if message.startswith("ACK"):
-                if self.rs_flag == False:
-                    self.rs_flag = True
+                if self.expecting_rs_ack.value:
+                    self.expecting_rs_ack.value = False
                     self.logger.debug("ACK for RS00 from STM32 received.")
                     continue
                 try:
@@ -360,6 +360,10 @@ class RaspberryPi:
             stm32_prefixes = ("FS", "BS", "FW", "BW", "FL", "FR", "BL",
                               "BR", "TL", "TR", "A", "C", "DT", "STOP", "ZZ", "RS")
             if command.startswith(stm32_prefixes):
+                if command.startswith("RS"):
+                    self.expecting_rs_ack.value = True
+                else:
+                    self.expecting_rs_ack.value = False
                 self.stm_link.send(command)
                 self.logger.debug(f"Sending to STM32: {command}")
 
@@ -446,82 +450,16 @@ class RaspberryPi:
         url = f"http://{API_IP}:{IMAGE_API_PORT}/image"
         filename = f"{int(time.time())}_{obstacle_id}_{signal}.jpg"
 
-        # con_file = "PiLCConfig9.txt"
-        # Home_Files = []
-        # Home_Files.append(os.getlogin())
-        # config_file = "/home/" + Home_Files[0] + "/" + con_file
-
-        # extns = ['jpg', 'png', 'bmp', 'rgb', 'yuv420', 'raw']
-        # shutters = [
-        #     -2000, -1600, -1250, -1000, -800, -640, -500, -400, -320, -288, -250, -240, -200, -160, -144, -125, -120, -
-        #     100, -96, -80, -60, -50, -48, -40, -30, -25, -20, -15, -13, -10, -8, -6, -5, -4, -3, 0.4, 0.5, 0.6, 0.8, 1,
-        #     1.1, 1.2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 20, 25, 30, 40, 50, 60, 75, 100, 112, 120, 150, 200, 220, 230,
-        #     239, 435]
-        # meters = ['centre', 'spot', 'average']
-        # awbs = ['off', 'auto', 'incandescent', 'tungsten',
-        #         'fluorescent', 'indoor', 'daylight', 'cloudy']
-        # denoises = ['off', 'cdn_off', 'cdn_fast', 'cdn_hq']
-
-        # config = []
-        # with open(config_file, "r") as file:
-        #     line = file.readline()
-        #     while line:
-        #         config.append(line.strip())
-        #         line = file.readline()
-        #     config = list(map(int, config))
-        # mode = config[0]
-        # speed = config[1]
-        # gain = config[2]
-        # brightness = config[3]
-        # contrast = config[4]
-        # red = config[6]
-        # blue = config[7]
-        # ev = config[8]
-        # extn = config[15]
-        # saturation = config[19]
-        # meter = config[20]
-        # awb = config[21]
-        # sharpness = config[22]
-        # denoise = config[23]
-        # quality = config[24]
-
         retry_count = 0
+        current_shutter_us = 20000
+        max_shutter_us = 120000
+        min_shutter_us = 8000
+        increase_factor = 1.6
+        decrease_factor = 0.6
 
         while True:
 
             retry_count += 1
-
-            # shutter = shutters[speed]
-            # if shutter < 0:
-            #     shutter = abs(1/shutter)
-            # sspeed = int(shutter * 1000000)
-            # if (shutter * 1000000) - int(shutter * 1000000) > 0.5:
-            #     sspeed += 1
-
-            # rpistr = "libcamera-still -e " + \
-            #     extns[extn] + " -n -t 500 -o " + filename
-            # rpistr += " --brightness " + \
-            #     str(brightness/100) + " --contrast " + str(contrast/100)
-            # rpistr += " --shutter " + str(sspeed)
-            # if ev != 0:
-            #     rpistr += " --ev " + str(ev)
-            # if sspeed > 1000000 and mode == 0:
-            #     rpistr += " --gain " + str(gain) + " --immediate "
-            # else:
-            #     rpistr += " --gain " + str(gain)
-            #     if awb == 0:
-            #         rpistr += " --awbgains " + str(red/10) + "," + str(blue/10)
-            #     else:
-            #         rpistr += " --awb " + awbs[awb]
-            # rpistr += " --metering " + meters[meter]
-            # rpistr += " --saturation " + str(saturation/10)
-            # rpistr += " --sharpness " + str(sharpness/10)
-            # rpistr += " --quality " + str(quality)
-            # rpistr += " --denoise " + denoises[denoise]
-            # rpistr += " --metadata - --metadata-format txt >> PiLibtext.txt"
-
-            # rpistr = f"libcamera-still -e jpg -n -t 500 -o {filename}"  # TODO: configure camera settings above later
-            # rpistr =
 
             rpi_str_parts = [
                 "raspistill",
@@ -533,7 +471,7 @@ class RaspberryPi:
                 "-sh", "40",
                 "-ISO", "100",
                 "-awb", "auto",
-                "-ss", "20000",
+                "-ss", str(current_shutter_us),
                 "-br", "50",
                 "-co", "10",
                 "-sa", "10",
@@ -541,12 +479,16 @@ class RaspberryPi:
             ]
             rpistr = " ".join(rpi_str_parts)
 
+            self.logger.debug(
+                "Attempt %d capturing with shutter %dus", retry_count, current_shutter_us
+            )
             os.system(rpistr)
 
             self.logger.debug("Requesting from image API")
 
-            response = requests.post(
-                url, files={"file": (filename, open(filename, 'rb'))})
+            with open(filename, 'rb') as image_file:
+                response = requests.post(
+                    url, files={"file": (filename, image_file)})
 
             if response.status_code != 200:
                 self.logger.error(
@@ -562,11 +504,23 @@ class RaspberryPi:
             elif retry_count > 3:
                 self.logger.info(f"Image recognition results: {results}")
                 self.logger.info("Recapturing with lower shutter speed...")
-                # speed -= 1
+                current_shutter_us = max(
+                    int(current_shutter_us * decrease_factor),
+                    min_shutter_us,
+                )
+                self.logger.debug(
+                    "Lowering shutter for next attempt to %dus", current_shutter_us
+                )
             elif retry_count <= 3:
                 self.logger.info(f"Image recognition results: {results}")
                 self.logger.info("Recapturing with higher shutter speed...")
-                # speed += 1
+                current_shutter_us = min(
+                    int(current_shutter_us * increase_factor),
+                    max_shutter_us,
+                )
+                self.logger.debug(
+                    "Increasing shutter for next attempt to %dus", current_shutter_us
+                )
 
         # release lock so that bot can continue moving
         self.movement_lock.release()
@@ -751,6 +705,7 @@ class RaspberryPi:
         try:
             if not self.command_queue.empty():
                 self.logger.info("Gyro reset! (auto-start)")
+                self.expecting_rs_ack.value = True
                 self.stm_link.send("RS00")
                 self.unpause.set()
                 self.android_queue.put(AndroidMessage('status', 'running'))
